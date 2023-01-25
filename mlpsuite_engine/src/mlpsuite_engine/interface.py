8fa33c297790b9dcf71966e7db88839598707e77
bubikhonza <37843347+bubikhonza@users.noreply.github.com>
2023-01-25 23:23:09 +0100

    model versioning 1.0

--- mlpsuite_engine/src/mlpsuite_engine/interface.py
+++ mlpsuite_engine/src/mlpsuite_engine/interface.py
@@ -6,11 +6,17 @@ from mlpsuite_engine.data_loader import DataLoader
 from pyspark.sql import DataFrame
 from pyspark.sql.streaming import DataStreamWriter, DataStreamReader
 import pyspark.sql.functions as F
+from datetime import datetime
 import yaml
 import logging
+import os, glob
+import random
+import string
 
 
 class Interface:
+    DT_FORMAT = "%Y%m%d%H%M_"
+
     def __init__(self, config_path: str):
         self.__config = DataLoader.load_yml(config_path)
         self.__stages_conf = self.__config["stages"]
@@ -64,7 +70,7 @@ class Interface:
     def __build_kafka_output(self, spark: SparkSession, result: DataFrame) -> DataStreamWriter:
         query = result \
             .writeStream \
-            .format("kafka") \
+            .format("kafka")
 
         for k, v in self.__output_kafka_settings.items():
             query.option(k, v)
@@ -75,14 +81,25 @@ class Interface:
 
         pipeline = self.__create_pipeline()
         fitted_pipeline = pipeline.fit(self.__load_train_data())
-        fitted_pipeline.save(self.__train_conf["pipeline_output"])
+        output_path = os.path.join(self.__train_conf["pipeline_output"],
+                                   str(datetime.now().strftime(Interface.DT_FORMAT)) + ''.join(
+                                       random.choices(string.ascii_uppercase + string.digits, k=3)))
+        fitted_pipeline.save(output_path)
         spark.stop()
 
     def run_predict(self) -> None:
         logger = logging.getLogger('pyspark')
         logger.error("My test info statement")
         spark = SparkSession.builder.appName("Pipeliner").getOrCreate()
-        pipeline = PipelineModel.load(self.__predict_conf["pipeline_path"])
+        version = str(self.__predict_conf.get("version", ""))
+        if version:
+            model_path = os.join(self.__predict_conf["pipeline_path"], version)
+        else:
+            root_dir = self.__predict_conf["pipeline_path"]
+            latestdir = max([os.path.join(root_dir, d) for d in os.listdir(root_dir)], key=os.path.getmtime)
+            model_path = os.join(self.__predict_conf["pipeline_path"], latestdir)
+
+        pipeline = PipelineModel.load(model_path)
         input_stream_df = self.__build_kafka_input(spark)
 
         schema = self.__create_pyspark_predict_schema()
@@ -93,7 +110,7 @@ class Interface:
         df = df.select("sample.*")
         result = pipeline.transform(df)
         result = result.select([F.col(c).cast("string") for c in result.columns])
-        result = result.withColumn("value", F.to_json(F.struct("*")).cast("string"),)
+        result = result.withColumn("value", F.to_json(F.struct("*")).cast("string"), )
         query = self.__build_kafka_output(spark, result)
 
         query.awaitTermination()
