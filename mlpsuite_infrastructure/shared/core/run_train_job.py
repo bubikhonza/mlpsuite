b8c17df2eb223dd81c1af5b9db1bab9fa278a67d
bubikhonza <37843347+bubikhonza@users.noreply.github.com>
2023-02-08 00:46:14 +0100

    GKE update

--- mlpsuite_infrastructure/shared/core/run_train_job.py
+++ mlpsuite_infrastructure/shared/core/run_train_job.py
@@ -1,7 +1,7 @@
 from mlpsuite_engine.interface import Interface
 
+# TODO: load path from env variable
 interface = Interface("/shared/usr/pipeline.yml")
-
 
 if __name__ == "__main__":
     interface.run_train()
