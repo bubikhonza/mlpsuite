915088a834aadb5d8ac4e624c41aa9cd611c84dd
bubikhonza <37843347+bubikhonza@users.noreply.github.com>
2023-02-22 15:35:27 +0100

    removed non subrepos

--- mlpsuite_infrastructure/README.md
+++
@@ -1,5 +0,0 @@
-## MLPSuite - Infrastructure
-Container orchestration with spark and kafka.
-
-- [docker-compose](docker-compose)
-- [kubernetes]() # In development
--- mlpsuite_infrastructure/docker-compose/shared/run_predict_job.py
+++
@@ -1,7 +0,0 @@
-from mlpsuite_engine.interface import Interface
-
-# TODO: load path from env variable
-interface = Interface("/shared/mlpsuite_engine_config.yaml")
-
-if __name__ == "__main__":
-    interface.run_predict()
